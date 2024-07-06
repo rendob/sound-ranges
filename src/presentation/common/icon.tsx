c426ae7c30d3990a627986516a06a3a334e02e89
rendob <55734639+rendob@users.noreply.github.com>
2024-07-06 22:50:24 +0900

    add icons to appBar

---
+++ src/presentation/common/icon.tsx
@@ -0,0 +1,38 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+import { appColor } from "../style/appColor";
+import React from "react";
+
+const styles = {
+  root: css({
+    cursor: "pointer",
+    padding: "8px",
+    ":hover": {
+      backgroundColor: appColor.hover(appColor.background),
+    },
+  }),
+};
+
+type Props = {
+  onClick: () => void;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const Icon = ({ onClick, className, children }: Props) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+      className={className}
+      css={styles.root}
+    >
+      {children}
+    </svg>
+  );
+};
