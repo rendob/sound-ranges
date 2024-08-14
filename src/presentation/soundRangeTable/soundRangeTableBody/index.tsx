e6459f2048ae7596dacf9a2c6ff7eefcec1c5966
rendob <55734639+rendob@users.noreply.github.com>
2024-08-14 20:14:33 +0900

    add background guidelines

--- src/presentation/soundRangeTable/soundRangeTableBody/index.tsx
+++ src/presentation/soundRangeTable/soundRangeTableBody/index.tsx
@@ -3,10 +3,31 @@ import { dispatch } from "../../../infrastructure/zustand/appStore";
 import { clearKeyboardSelection } from "../../../infrastructure/zustand/keyboard/action";
 import { SoundRangeItem } from "./soundRangeItem";
 import { useInstrumentIds } from "../../../infrastructure/zustand/instruments/selector";
+import { appColor } from "../../style/appColor";
+import { appDimen } from "../../style/appDimen";
+
+const getTableBackground = () => {
+  const borderColor = `${appColor.border}40`;
+  const borderWidth = 1;
+  const leftBoundary = borderWidth / 2;
+  const rightBoundary = appDimen.keyboardKeyWidth - borderWidth / 2;
+  return `
+    repeating-linear-gradient(
+      to right,
+      ${borderColor},
+      ${borderColor} ${leftBoundary}px,
+      transparent ${leftBoundary}px,
+      transparent ${rightBoundary}px,
+      ${borderColor} ${rightBoundary}px,
+      ${borderColor} ${appDimen.keyboardKeyWidth}px
+    )
+  `.trim();
+};
 
 const styles = {
   root: (width: number) =>
     css({
+      background: getTableBackground(),
       flex: 1,
       position: "relative",
       width: width,
