b60aab54b5415e883356a4638613194d38d1b40d
rendob <55734639+rendob@users.noreply.github.com>
2024-08-17 18:40:40 +0900

    automatically detect browser language

--- src/i18n/configs.ts
+++ src/i18n/configs.ts
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import translation_en from "./en.json";
 import translation_ja from "./ja.json";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 export const langCodes = ["en", "ja"] as const;
 export type LangCode = (typeof langCodes)[number];
@@ -14,12 +15,16 @@ export const i18nResources = {
   },
 } as const satisfies { [key in LangCode]: unknown };
 
-i18next.use(initReactI18next).init({
-  resources: i18nResources,
-  lng: "ja",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: langCodes,
+    resources: i18nResources,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18next;
