9fa43be0d32d3034f9dfae1c3dd5943979df50ce
rendob <55734639+rendob@users.noreply.github.com>
2025-12-02 15:32:08 +0900

    style: format files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    App
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>App</StrictMode>,
+);
