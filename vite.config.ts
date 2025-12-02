00d6c0fae88e6605bdc72195cd3f47685dc39fc9
rendob <55734639+rendob@users.noreply.github.com>
2025-12-02 15:46:12 +0900

    test: add tests for exists function

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest/config" />
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    include: ["src/**/*.ts"],
+  },
+  define: {
+    "import.meta.vitest": "undefined",
+  },
 });
