6f3ea6f4488b0c7fe5b54a6c618197cf19295d5d
rendob <55734639+rendob@users.noreply.github.com>
2025-12-02 16:13:50 +0900

    feat: create Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,14 @@
+import { Button, ButtonVariant } from "../_components/Button";
+
 export const App: React.FC = () => {
-  return <main className="p-4">App</main>;
+  return (
+    <main className="p-4">
+      App
+      <div className="space-x-2">
+        <Button variant={ButtonVariant.FILLED}>Filled</Button>
+        <Button variant={ButtonVariant.OUTLINED}>Outlined</Button>
+        <Button variant={ButtonVariant.DESTRUCTIVE}>Destructive</Button>
+      </div>
+    </main>
+  );
 };
