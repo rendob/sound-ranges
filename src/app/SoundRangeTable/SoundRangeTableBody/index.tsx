ae5d15aa6db3d00e6ebbd765d4205e89ec0d347f
rendob <55734639+rendob@users.noreply.github.com>
2025-12-04 21:36:34 +0900

    clear selection on background click

--- src/app/SoundRangeTable/SoundRangeTableBody/index.tsx
+++ src/app/SoundRangeTable/SoundRangeTableBody/index.tsx
@@ -1,16 +1,23 @@
 import { allMidiProgramNumbers } from "@/_features/instrument/midiProgramNumber";
 import { allNoteNumbers } from "@/_features/noteNumber/model";
+import { pianoKeyStore } from "@/_features/pianoKey/store";
 import { createTableBackground } from "./createTableBackground";
 import { SoundRangeItem } from "./SoundRangeItem";
 
 export const SoundRangeTableBody: React.FC = () => {
+  const handleClick: React.MouseEventHandler = () => {
+    pianoKeyStore.clearSelection();
+  };
+
   return (
-    <div
+    <button
       className="h-full overflow-y-scroll pt-1"
       style={{
         width: `calc(var(--piano-key-width)*${allNoteNumbers.length})`,
         background: createTableBackground(),
       }}
+      type="button"
+      onClick={handleClick}
     >
       {allMidiProgramNumbers.map((midiProgramNumber) => (
         <SoundRangeItem
@@ -18,6 +25,6 @@ export const SoundRangeTableBody: React.FC = () => {
           midiProgramNumber={midiProgramNumber}
         />
       ))}
-    </div>
+    </button>
   );
 };
