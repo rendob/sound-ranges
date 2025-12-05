12b4c6bbc6b68b65ce4b2bfa2bd119176053b470
rendob <55734639+rendob@users.noreply.github.com>
2025-12-05 12:06:25 +0900

    ci: only deploy in develop, main, or PR branch

---
+++ .vercel/ignore-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_PULL_REQUEST_ID: $VERCEL_GIT_PULL_REQUEST_ID"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_PULL_REQUEST_ID" != "" ]]; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0
+fi
