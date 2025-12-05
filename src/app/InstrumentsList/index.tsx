5c24d18fe1603c53ce8814ec81c9e27edb04a617
rendob <55734639+rendob@users.noreply.github.com>
2025-12-05 10:47:42 +0900

    fix: animate InstrumentsList in Safari

--- src/app/InstrumentsList/index.tsx
+++ src/app/InstrumentsList/index.tsx
@@ -12,8 +12,11 @@ export const InstrumentsList: React.FC<Props> = ({ isOpen }) => {
     <aside
       className={twMerge(
         "absolute top-10 z-10 flex h-[calc(100vh-40px)] max-w-[calc(var(--piano-key-width)*20)] flex-col border-border border-t border-r bg-base",
-        "starting:-translate-x-full transition-[display,translate] transition-discrete",
-        isOpen ? "translate-x-0" : "-translate-x-full hidden",
+        // translate のアニメーションは Safari で機能しなかったため transform で記述
+        "starting:transform-[translateX(-100%)] transition-[display,transform] transition-discrete",
+        isOpen
+          ? "transform-[translateX(0)]"
+          : "transform-[translateX(-100%)] hidden",
       )}
     >
       <AllItem />
